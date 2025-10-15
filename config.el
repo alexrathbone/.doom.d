143b771fd79ae58813cf874b065ffb9599114782
Alex Rathbone <alex@rathbone.me>
2025-10-15 22:48:08 +0100

    Adopted tree-sitter & tidied config files

--- config.el
+++ config.el
@@ -3,15 +3,34 @@
 (setq user-full-name "Alex Rathbone"
       user-mail-address "alex@rathbone.me")
 
-(setq doom-theme 'doom-gruvbox)
+;;;; UI
+;; Theme & fonts
+(setq doom-theme 'doom-gruvbox
+      doom-font (font-spec :family "JetBrains Mono" :size 13)
+      doom-variable-pitch-font (font-spec :family "DejaVu sans" :size 13))
 
-(after! company
-  (setq company-idle-delay nil))
+;; Hide line numbers
+(setq display-line-numbers-type nil)
 
+;;;; Modules
+
+;;; :completion corfu
+;; Disable auto completion
+(after! corfu
+	(setq corfu-auto nil))
+
+;;; :editor evil
+;; Focus new window after splitting
+(setq evil-split-window-below t
+      evil-vsplit-window-right t)
+
+;;; :tools lsp
+(setq lsp-lens-enable nil)
 (setq lsp-clients-clangd-args '("--header-insertion=never"))
 
+;;; :lang org
 (setq org-directory "~/org/")
-(setq display-line-numbers-type nil)
+
 (setq projectile-project-search-path '("~/code/"))
 (setq c-default-style "k&r"
       c-basic-offset 4)
